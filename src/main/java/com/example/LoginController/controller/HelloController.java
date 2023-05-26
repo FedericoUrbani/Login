fadd4dbdac9f3387619628fb42f4dbca9e5ed7a4
federico urbani <urbanifederico5@gmail.com>
2023-05-26 20:08:04 +0200

    updated main

---
+++ src/main/java/com/example/LoginController/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.LoginController.controller;
+
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String helloController(){
+        return "hello";
+    }
+
+    @GetMapping("/admin")
+    @Secured("/ADMIN")
+    public String helloAdmin(){
+        return "admin";
+    }
+}
